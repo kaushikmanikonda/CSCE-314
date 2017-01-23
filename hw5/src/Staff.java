bf847cd5492d6be998c4e25dcaec333381563825
umutyerli <umut@yerli.org>
2017-01-23 16:13:23 -0600

    first commit

---
+++ hw5/src/Staff.java
@@ -0,0 +1,27 @@
+// Umut Yerli
+// 924001029
+// Recieved help from peer teachers
+
+
+
+public class Staff extends Employee {
+
+    protected String title;
+
+    public Staff(String name, String title) {
+        super(name);
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String toString() {
+        return "Name: " + getName() + " Title: " + getTitle();
+    }
+}
---
+++ hw5/src/Student.java
@@ -0,0 +1,32 @@
+// Umut Yerli
+// 924001029
+// Recieved help from peer teachers
+
+
+public class Student extends Person {
+
+    protected String status;
+
+    public Student(String name, String status) {
+        super(name);
+        this.status = status;
+    }
+    
+    public void setName(String name) {
+    	this.name = name;
+    }
+    public String getStatus() {
+    	return this.status;
+    }
+    
+    public void setStatus(String status) {
+    	this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + getName() + " Class: " + getStatus();
+    }
+}
+
+
